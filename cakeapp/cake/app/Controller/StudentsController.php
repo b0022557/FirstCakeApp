2caca8878224ee50c6f5b9cba290259f4402d5d6
Ian Ibbotson <i.ibbotson@shu.ac.uk>
2012-02-06 10:56:24 +0000

    Added config

--- cakeapp/cake/app/Controller/StudentsController.php
+++ cakeapp/cake/app/Controller/StudentsController.php
@@ -1,9 +1,10 @@
 <?php
 class StudentsController extends AppController {
-    public $name = 'Students';
-    public $helpers = array('Html', 'Form');
+    public $scaffold;
+    # public $name = 'Students';
+    # public $helpers = array('Html', 'Form');
 
-    public function index() {
-        $this->set('students', $this->Student->find('all'));
-    }
+    # public function index() {
+    #     $this->set('students', $this->Student->find('all'));
+    # }
 }
