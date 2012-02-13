7af7e1dff36a9b823b2f62a9732e26ee5e8642d6
Ian Ibbotson <i.ibbotson@shu.ac.uk>
2012-02-13 10:52:11 +0000

    Added Courses Controller

---
+++ cakeapp/cake/app/Controller/CoursesController.php
@@ -0,0 +1,10 @@
+<?php
+class CoursesController extends AppController {
+    public $scaffold;
+    # public $name = 'Students';
+    # public $helpers = array('Html', 'Form');
+
+    # public function index() {
+    #     $this->set('students', $this->Student->find('all'));
+    # }
+}
