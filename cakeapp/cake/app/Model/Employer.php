678782844338d610aa152a2ac4e073dd1c621835
Ian Ibbotson <i.ibbotson@shu.ac.uk>
2012-02-13 08:48:37 +0000

    Added hasMany between employer and jobdescription

--- cakeapp/cake/app/Model/Employer.php
+++ cakeapp/cake/app/Model/Employer.php
@@ -1,4 +1,12 @@
 <?php
 class Employer extends AppModel {
     public $name = 'Employer';
+
+    var $hasMany = array(
+        'Jobdescription' => array(
+            'className'     => 'Jobdescription',
+            'foreignKey'    => 'employers_id',
+            'dependent'     => true
+        )    
+    );  
 }
